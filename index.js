81582f7ca1aeff75d7e8270dec6ec6a962f2035e
Dustin Van Tate Testa <toast27@gmail.com>
2019-10-16 15:01:54 -0500

    quick and simple :)

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+function localize(d, tz) {
+	// this is ugly solution but works without having to use another library
+	return new Date(
+		d.toLocaleString("en-US", { 
+			timeZone: "America/New_York"} ));
+}
+
+function weekday(d) {
+	const dayNames = [
+		"Monday", "Tuesday", "Wednesday", "Thursday", 
+		"Friday", "Saturday", "Sunday"
+	];
+	return dayNames[d.getDay()];
+}
+
+// normal operations
+app.post("/invoke/realtime", (req, res) => {
+	const tz = req.body.arguments.Timezone;
+	res.json(weekday(localize(new Date(), tz)));
+});
+
+// used for backtesting
+app.post("/invoke/timeline", (req, res) => {
+	const tz = req.body.arguments.Timezone;
+
+	// localize dates for start and end of period
+	const [ start, end ] = req.body.period.map(d => localize(new Date(d), tz));
+
+	// this will be our timeline
+	let ret = {};
+
+	// day of week at start of period
+	ret["start"] = weekday(start);
+
+	let ts = start;
+
+	// truncate to date, convert to epoch milliseconds
+	ts = ts.setUTCHours(0, 0, 0, 0);
+
+	// ms/day
+	const oneDay = 1000 * 60 * 60 * 24;
+	ts += oneDay;
+
+	// enter weekdays for each day in period
+	while (ts < end) {
+		const d = new Date(ts);
+		ret[d] = weekday(d);
+		ts += oneDay;
+	}
+
+
+	res.json({
+		timeline: ret,
+		warn: [], // nothing bad happened
+		resolutions: [] // nothings for user to click on
+	});
+});
+
+app.listen(5051, () => 
+    console.log("Extension Server Started successfully"))
